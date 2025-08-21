e6f75efdf57d28d66a6c5006e82c1cb0af903ba2
ismacm1 <isma.crm.1@gmail.com>
2025-08-21 15:44:56 -0500

    Avances_21_agosto

---
+++ javascript-3146153/4_audio_video_js/app.js
@@ -0,0 +1,12 @@
+let video = document.querySelector("#video");
+let playVideo = document.querySelector("#play-video");
+let stopVideo = document.querySelector("#pause-video");
+let image1 = document.querySelector("#image1 img");
+let PlayStopImage1 = document.querySelector("#play-stop-img1");
+let PlayStopImage2 = document.querySelector("#play-stop-img2");
+
+console.log(video);
+console.log(image1);
+console.log(PlayStopImage1);
+console.log(PlayStopImage2);
+console.log(PlayStopImage1);
